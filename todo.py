1bb0706cbb3002ff084667b4648b40f9f66271fc
Laweens <laweensfrancois@gmail.com>
2023-12-21 10:51:32 -0500

    fin thursday

---
+++ todo.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template , request
+app = Flask(__name__)
+
+todo = ['Pass all my classes for the year', 'Graduate Highschool']
+
+@app.route('/', methods=['GET','POST'])
+def index():
+    new_todo = request.form['new_todo']
+    todo.append(new_todo)
+    return render_template( 
+        'todo.html.jinja', todos= todo
+                      
+        )
