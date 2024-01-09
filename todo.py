5cdba61bc2a5fbe90e2a723bd08ab7d3f5432baf
Laweens <laweensfrancois@gmail.com>
2024-01-09 10:21:33 -0500

    update to sql

--- todo.py
+++ todo.py
@@ -24,14 +24,19 @@ def index():
     if request.method == 'POST':
         new_todo = request.form['new_todo']
         todo.append(new_todo)
-    
+        cursor = connect.cursor()
+        cursor.execute(f"INSERT INTO `todos` (description) VALUES ('{new_todo}')")
+        cursor.close
+
+
     cursor = connect.cursor()
-    cursor.execute("SELECT `description` FROM `todos`")
+    cursor.execute("SELECT * FROM `todos`")
     results = cursor.fetchall()
-                      
+    cursor.close      
+
     return render_template( 
         'todo.html.jinja', 
-        todos= todo
+        todos= results
         )
 
 @app.route('/delete_todo/<int:todo_index>', methods =['POST'])
